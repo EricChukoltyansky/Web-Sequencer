8b731c69e9c7beea3f07fc8f6b6dd5047a7bbc50
Idan Lev <idan.93lev@gmail.com>
2022-09-05 12:15:47 +0300

    added Power On button

---
+++ client/src/Components/Buttons/PowerOn.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { ImVolumeMedium } from "react-icons/im";
+
+const Style = {
+  PowerOn: styled.button`
+  padding: 0.75em 0.90em;
+  outline: none;
+  background: radial-gradient(#b1f5d8, #0b996a);
+  border: none;
+  border-radius: 5px;
+  color: white;
+  font-size: 1em;
+  margin: 0.5em;
+  transition: all 0.2s;
+
+  &:hover {
+    border-radius: 50px;
+    cursor: pointer;
+  }
+`,
+};
+
+const PowerOn = ({ onClick }: {onClick: () => void}) => (
+  <Style.PowerOn onClick={onClick}>
+    {<ImVolumeMedium />}
+  </Style.PowerOn>
+);
+
+export default PowerOn;
